a4ce691553b3de374e53fb61ecfed9df77289385
Kevin King <4kevinking@gmail.com>
2016-12-02 10:56:04 -0500

    optimize imports

--- app/src/main/java/co/krypt/kryptonite/MainActivity.java
+++ app/src/main/java/co/krypt/kryptonite/MainActivity.java
@@ -1,24 +1,18 @@
 package co.krypt.kryptonite;
 
-import android.net.Uri;
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
