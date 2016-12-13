564af5f6b907684578c8be60b32d125178ccc487
Kevin King <4kevinking@gmail.com>
2016-12-13 12:47:37 -0500

    add missing files

---
+++ app/src/main/java/co/krypt/kryptonite/me/MeFragment.java
@@ -0,0 +1,35 @@
+package co.krypt.kryptonite.me;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.krypt.kryptonite.R;
+
+public class MeFragment extends Fragment {
+    public MeFragment() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_me, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
