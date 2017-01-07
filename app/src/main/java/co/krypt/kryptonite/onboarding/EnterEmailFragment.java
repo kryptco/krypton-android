ad30a80ff4f486808165bf2fe624578086010fa0
Kevin King <4kevinking@gmail.com>
2017-01-07 11:49:37 -0500

    add identicon to EnterEmailFragment

--- app/src/main/java/co/krypt/kryptonite/onboarding/EnterEmailFragment.java
+++ app/src/main/java/co/krypt/kryptonite/onboarding/EnterEmailFragment.java
@@ -1,15 +1,32 @@
 package co.krypt.kryptonite.onboarding;
 
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.docuverse.identicon.IdenticonUtil;
+import com.docuverse.identicon.NineBlockIdenticonRenderer;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.security.InvalidKeyException;
+
 import co.krypt.kryptonite.R;
+import co.krypt.kryptonite.crypto.KeyManager;
+import co.krypt.kryptonite.crypto.SHA256;
+import co.krypt.kryptonite.crypto.SSHKeyPair;
+import co.krypt.kryptonite.exception.CryptoException;
+import co.krypt.kryptonite.uiutils.MLRoundedImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,6 +50,18 @@ public class EnterEmailFragment extends Fragment {
                 next();
             }
         });
+        MLRoundedImageView identiconImage = (MLRoundedImageView) root.findViewById(R.id.identicon);
+        try {
+            NineBlockIdenticonRenderer renderer = new NineBlockIdenticonRenderer();
+            renderer.setBackgroundColor(Color.TRANSPARENT);
+            renderer.setPatchSize(80);
+            BigInteger hash = new BigInteger(SHA256.digest(new byte[]{0}));
+            Bitmap identicon = renderer.render(hash, 2000);
+            identiconImage.setImageBitmap(identicon);
+            SSHKeyPair keyPair = KeyManager.loadOrGenerateKeyPair(KeyManager.MY_RSA_KEY_TAG);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return root;
     }
 
---
+++ app/src/main/java/co/krypt/kryptonite/uiutils/MLRoundedImageView.java
@@ -0,0 +1,125 @@
+package co.krypt.kryptonite.uiutils;
+
+/**
+ *  https://github.com/androsland/MLRoundedImageView/blob/master/MLRoundedImageView.java
+ */
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class MLRoundedImageView extends ImageView {
+    Drawable drawable;
+    Bitmap bitmap;
+    Canvas c;
+
+    public MLRoundedImageView(Context context) {
+        super(context);
+        drawable = getDrawable();
+        if (drawable == null || drawable instanceof BitmapDrawable) {
+
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                    drawable.getIntrinsicHeight(), Config.ARGB_8888);
+            c = new Canvas(bitmap);
+        }
+    }
+
+    public MLRoundedImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        drawable = getDrawable();
+        if (drawable == null || drawable instanceof BitmapDrawable) {
+
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                    drawable.getIntrinsicHeight(), Config.ARGB_8888);
+            c = new Canvas(bitmap);
+        }
+    }
+
+    public MLRoundedImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        drawable = getDrawable();
+        if (drawable instanceof BitmapDrawable) {
+
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                    drawable.getIntrinsicHeight(), Config.ARGB_8888);
+            c = new Canvas(bitmap);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        Drawable drawable = getDrawable();
+
+        if (drawable == null) {
+            return;
+        }
+
+        if (getWidth() == 0 || getHeight() == 0) {
+            return;
+        }
+
+        Bitmap b;
+        if (drawable instanceof BitmapDrawable) {
+            b = ((BitmapDrawable) drawable).getBitmap();
+        } else {
+            drawable.setBounds(0, 0, c.getWidth(), c.getHeight());
+            drawable.draw(c);
+            b = bitmap;
+        }
+
+        Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
+
+        int w = getWidth(), h = getHeight();
+
+        Bitmap roundBitmap = getCroppedBitmap(bitmap, w);
+        canvas.drawBitmap(roundBitmap, 0, 0, null);
+
+    }
+
+    public static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
+        Bitmap sbmp;
+
+        if (bmp.getWidth() != radius || bmp.getHeight() != radius) {
+            float smallest = Math.min(bmp.getWidth(), bmp.getHeight());
+            float factor = smallest / radius;
+            sbmp = Bitmap.createScaledBitmap(bmp,
+                    (int) (bmp.getWidth() / factor),
+                    (int) (bmp.getHeight() / factor), false);
+        } else {
+            sbmp = bmp;
+        }
+
+        Bitmap output = Bitmap.createBitmap(radius, radius, Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xffa19774;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, radius, radius);
+
+        paint.setAntiAlias(true);
+        paint.setFilterBitmap(true);
+        paint.setDither(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(Color.parseColor("#BAB399"));
+        canvas.drawCircle(radius / 2 + 0.7f, radius / 2 + 0.7f,
+                radius / 2 + 0.1f, paint);
+        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+        canvas.drawBitmap(sbmp, rect, rect, paint);
+
+        return output;
+    }
+}
