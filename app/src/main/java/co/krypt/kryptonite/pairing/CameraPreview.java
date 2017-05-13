fc477838fbf921b93b39bd65d9da951f1783229d
Kenny Levinsen <w@kl.wtf>
2017-05-13 14:30:24 +0200

    Instant camera setup/teardown on visibility hint
    Move all setup/teardown work to a separate thread, using a threadpool
    with a single thread to synchronize start/stop operations. This
    frees up the UI thread, making the UI more fluid.

--- app/src/main/java/co/krypt/kryptonite/pairing/CameraPreview.java
+++ app/src/main/java/co/krypt/kryptonite/pairing/CameraPreview.java
@@ -2,13 +2,13 @@ package co.krypt.kryptonite.pairing;
 
 
 import android.content.Context;
+import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
-import java.util.List;
 
 /** A basic Camera preview class https://developer.android.com/guide/topics/media/camera.html#preview-layout */
 
@@ -26,36 +26,27 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // underlying surface is created and destroyed.
         mHolder = getHolder();
         mHolder.addCallback(this);
-        // deprecated setting, but required on Android versions prior to 3.0
-        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
-    public void surfaceCreated(SurfaceHolder holder) {
-        Log.d(TAG, "surfaceCreated");
-    }
+    public void surfaceCreated(SurfaceHolder holder) {}
+    public void surfaceDestroyed(SurfaceHolder holder) {}
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {}
 
-    public void surfaceDestroyed(SurfaceHolder holder) {
-    }
-
-    public void setCamera(Camera camera) {
+    public void setup(Camera camera) {
         // The Surface has been created, now tell the camera where to draw the preview.
         try {
             camera.setPreviewDisplay(mHolder);
-            camera.startPreview();
-            List<String> focusModes = camera.getParameters().getSupportedFocusModes();
-            if (focusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
-                Camera.Parameters params = camera.getParameters();
-                params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
-                camera.setParameters(params);
-            }
             previewSize = camera.getParameters().getPreviewSize();
         } catch (IOException e) {
             Log.d(TAG, "Error setting camera preview: " + e.getMessage());
         }
     }
 
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        return;
+    public void clear() {
+        // This trick rebuilds the surface, making it black again. Starting with a black surface
+        // while the camera loads looks nicer than having an odd frozen image from previous run.
+        mHolder.setFormat(PixelFormat.TRANSPARENT);
+        mHolder.setFormat(PixelFormat.OPAQUE);
     }
 
     public Camera.Size getPreviewSize() {
