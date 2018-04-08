f21dcb8ca4d3095451c9a25cd7e47ef0ecad4e8c
Kevin King <kevin@krypt.co>
2018-04-08 13:14:49 -0400

    Fix potential create team NPE

--- app/src/main/java/co/krypt/krypton/team/onboarding/create/OnboardingVerifyEmailFragment.java
+++ app/src/main/java/co/krypt/krypton/team/onboarding/create/OnboardingVerifyEmailFragment.java
@@ -21,6 +21,8 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import co.krypt.krypton.R;
 import co.krypt.krypton.exception.CryptoException;
 import co.krypt.krypton.me.MeStorage;
@@ -36,9 +38,13 @@ public class OnboardingVerifyEmailFragment extends Fragment {
 
     private final String TAG = "OnboardingVerifyEmail";
 
-    private CreateTeamProgress progress;
-
     private AppCompatEditText emailEditText;
+
+    private ProgressBar progressBar;
+    private Button cancelButton;
+    private Button changeButton;
+    private Button resendButton;
+    ConstraintLayout checkYourEmailContainer;
 
     public OnboardingVerifyEmailFragment() {
     }
@@ -56,106 +62,43 @@ public class OnboardingVerifyEmailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_teams_onboarding_verify_email, container, false);
-        Button cancelButton = rootView.findViewById(R.id.cancelButton);
+        cancelButton = rootView.findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(v -> {
-            progress.reset();
+            new CreateTeamProgress(v.getContext()).reset();
             getActivity().finish();
         });
 
-        ProgressBar progressBar = rootView.findViewById(R.id.progressBar);
+        progressBar = rootView.findViewById(R.id.progressBar);
         progressBar.setAlpha(0);
 
-        Button changeButton = rootView.findViewById(R.id.changeButton);
+        changeButton = rootView.findViewById(R.id.changeButton);
         changeButton.setEnabled(false);
-        Button resendButton = rootView.findViewById(R.id.resendButton);
+        resendButton = rootView.findViewById(R.id.resendButton);
         resendButton.setEnabled(false);
-        ConstraintLayout checkYourEmailContainer = rootView.findViewById(R.id.checkYourEmailContainer);
+        checkYourEmailContainer = rootView.findViewById(R.id.checkYourEmailContainer);
         checkYourEmailContainer.setAlpha(0);
 
         AppCompatTextView teamName = rootView.findViewById(R.id.teamName);
-        teamName.setText(progress.getTeamOnboardingData().name);
+        teamName.setText(new CreateTeamProgress(getContext()).getTeamOnboardingData().name);
 
         emailEditText = rootView.findViewById(R.id.profileEmail);
 
-        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-        Runnable focusEmail = () -> {
-            emailEditText.setEnabled(true);
-            emailEditText.requestFocus();
-            if (imm != null) {
-                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-            }
-            progressBar.animate().alpha(0).setDuration(500).start();
-        };
-        focusEmail.run();
-
-        Runnable requestEmailChallenge = () -> {
-            emailEditText.setEnabled(false);
-            emailEditText.clearFocus();
-            if (imm != null) {
-                imm.hideSoftInputFromWindow(emailEditText.getWindowToken(), 0);
-            }
-            progressBar.animate().alpha(1).setDuration(1000).start();
-
-            final String email = emailEditText.getEditableText().toString();
-
-            new Thread(() -> {
-                MeStorage meStorage = Silo.shared(getContext()).meStorage();
-                try {
-                    meStorage.setEmail(email);
-                } catch (CryptoException e) {
-                    Error.shortToast(getContext(), "Failed to set email: " + e.getMessage());
-                    e.printStackTrace();
-                    return;
-                }
-                Profile me = meStorage.load();
-                progress.updateTeamData((s, d) -> {
-                    if (s.equals(CreateStage.VERIFY_EMAIL) || s.equals(CreateStage.EMAIL_CHALLENGE_SENT)) {
-                        d.creatorProfile = me;
-                        Log.i(TAG, "requesting email challenge...");
-                        try {
-                            final Sigchain.NativeResult<JsonObject> result = TeamDataProvider.requestEmailChallenge(getContext(), email);
-                            emailEditText.post(() -> {
-                                if (result.success != null) {
-                                    Log.i(TAG, "request email challenge success");
-                                    progressBar.animate().alpha(0).setDuration(500).start();
-                                    checkYourEmailContainer.animate().alpha(1).setDuration(1000).start();
-                                    changeButton.setEnabled(true);
-                                    resendButton.setEnabled(true);
-                                } else {
-                                    Log.i(TAG, "request email challenge failure: " + result.error);
-                                    Error.shortToast(getContext(), "Error sending email: " + result.error);
-                                    focusEmail.run();
-                                }
-                            });
-                            if (result.success != null) {
-                                return CreateStage.EMAIL_CHALLENGE_SENT;
-                            } else {
-                                return s;
-                            }
-                        } catch (Native.NotLinked notLinked) {
-                            notLinked.printStackTrace();
-                        }
-                    }
-                    return s;
-                });
-            }).start();
-        };
         emailEditText.setOnEditorActionListener((v, i, ev) -> {
-            requestEmailChallenge.run();
+            EventBus.getDefault().post(new RequestEmailChallenge());
             return false;
         });
         emailEditText.setOnFocusChangeListener((view, b) -> {
             if (view.isEnabled() && !b) {
-                requestEmailChallenge.run();
+                EventBus.getDefault().post(new RequestEmailChallenge());
             }
         });
 
         resendButton.setOnClickListener(v -> {
-            requestEmailChallenge.run();
+            EventBus.getDefault().post(new RequestEmailChallenge());
         });
 
         changeButton.setOnClickListener(v -> {
-            focusEmail.run();
+            EventBus.getDefault().post(new FocusEmail());
         });
 
         Button submitButton = rootView.findViewById(R.id.submitButton);
@@ -173,15 +116,86 @@ public class OnboardingVerifyEmailFragment extends Fragment {
 
         EventBus.getDefault().register(this);
         EventBus.getDefault().post(new IdentityService.GetProfile(getContext()));
+
+        EventBus.getDefault().post(new FocusEmail());
         return rootView;
     }
 
+    private AtomicBoolean updatedInitialEmail = new AtomicBoolean(false);
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
     public void updateInitialEmail(IdentityService.GetProfileResult r) {
-        EventBus.getDefault().unregister(this);
-        if (emailEditText.getText().length() == 0 && r.profile != null) {
+        if (emailEditText.getText().length() == 0 && r.profile != null && !updatedInitialEmail.getAndSet(true)) {
             emailEditText.setText(r.profile.email);
         }
+    }
+
+    private static class FocusEmail {}
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void focusEmail(FocusEmail focusEmail) {
+        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        emailEditText.setEnabled(true);
+        emailEditText.requestFocus();
+        if (imm != null) {
+            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+        }
+        progressBar.animate().alpha(0).setDuration(500).start();
+    }
+
+    private static class RequestEmailChallenge {}
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void requestEmailChallenge(RequestEmailChallenge requestEmailChallenge) {
+        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        emailEditText.setEnabled(false);
+        emailEditText.clearFocus();
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(emailEditText.getWindowToken(), 0);
+        }
+        progressBar.animate().alpha(1).setDuration(1000).start();
+
+        final String email = emailEditText.getEditableText().toString();
+
+        new Thread(() -> {
+            MeStorage meStorage = Silo.shared(getContext()).meStorage();
+            try {
+                meStorage.setEmail(email);
+            } catch (CryptoException e) {
+                Error.shortToast(getContext(), "Failed to set email: " + e.getMessage());
+                e.printStackTrace();
+                return;
+            }
+            Profile me = meStorage.load();
+            new CreateTeamProgress(getContext()).updateTeamData((s, d) -> {
+                if (s.equals(CreateStage.VERIFY_EMAIL) || s.equals(CreateStage.EMAIL_CHALLENGE_SENT)) {
+                    d.creatorProfile = me;
+                    Log.i(TAG, "requesting email challenge...");
+                    try {
+                        final Sigchain.NativeResult<JsonObject> result = TeamDataProvider.requestEmailChallenge(getContext(), email);
+                        emailEditText.post(() -> {
+                            if (result.success != null) {
+                                Log.i(TAG, "request email challenge success");
+                                progressBar.animate().alpha(0).setDuration(500).start();
+                                checkYourEmailContainer.animate().alpha(1).setDuration(1000).start();
+                                changeButton.setEnabled(true);
+                                resendButton.setEnabled(true);
+                            } else {
+                                Log.i(TAG, "request email challenge failure: " + result.error);
+                                Error.shortToast(getContext(), "Error sending email: " + result.error);
+                                EventBus.getDefault().post(new FocusEmail());
+                            }
+                        });
+                        if (result.success != null) {
+                            return CreateStage.EMAIL_CHALLENGE_SENT;
+                        } else {
+                            return s;
+                        }
+                    } catch (Native.NotLinked notLinked) {
+                        notLinked.printStackTrace();
+                    }
+                }
+                return s;
+            });
+        }).start();
+
     }
 
     @Override
@@ -189,19 +203,7 @@ public class OnboardingVerifyEmailFragment extends Fragment {
         if (EventBus.getDefault().isRegistered(this)) {
             EventBus.getDefault().unregister(this);
         }
+        updatedInitialEmail.set(false);
         super.onDestroyView();
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-        progress = new CreateTeamProgress(context);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        progress = null;
     }
 }
